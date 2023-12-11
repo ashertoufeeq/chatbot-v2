9e154e9890fdb6632bfea6b239e3817f2f1f5aae
Faisal Manzer <faisal_manzer@yahoo.in>
2023-12-11 21:20:54 +0000

    feat: deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
-import { Col, Flex, Row, Typography } from 'antd';
-import { PlasmicRootProvider, } from "@plasmicapp/loader-react";
+// import { Col, Flex, Row, Typography } from 'antd';
+// import { PlasmicRootProvider, } from "@plasmicapp/loader-react";
 
 import './App.css';
 import 'react-chatbot-kit/build/main.css'
 import ChatBot from './components/chatbot';
-import background from './assets/background_graphic.jpg'
-import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
-import { PLASMIC } from './components/plasmic-init';
-import { CatchAllPage } from './components/catchAllPages';
+// import background from './assets/background_graphic.jpg'
+// import { BrowserRouter as Router, Routes, Route, } from 'react-router-dom';
+// import { PLASMIC } from './components/plasmic-init';
+// import { CatchAllPage } from './components/catchAllPages';
 
 
 function App() {
 
   return (<>
-    <PlasmicRootProvider loader={PLASMIC}>
+    {/* <PlasmicRootProvider loader={PLASMIC}>
       <Router>
         <Routes>
           <Route path="/" Component={CatchAllPage} />
         </Routes>
       </Router>
-    </PlasmicRootProvider>
+    </PlasmicRootProvider> */}
       <ChatBot/>
       </>
   );
